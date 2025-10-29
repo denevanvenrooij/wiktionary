7f820555cc7b2e099c9ce091e18d780611486b72
denevanvenrooij <denevanvenrooij@gmail.com>
2025-10-29 15:37:58 +0100

    inital commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import mediawikiapi
+
+function fetchWikiExtract(){
+    const = wikiEndpoint = 'https://simple.wikipedia.org/w/api.php';
+    const = wikiParams = '?action=query'
+    + '&prop=extract'
+}
